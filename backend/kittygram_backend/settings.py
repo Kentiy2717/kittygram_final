6c6182b3c32014abded0dd0cb630e605e9e37ecc
Innokentiy Motriy <79234101338@yandex.ru>
2024-08-03 07:49:41 +0700

    'Europe/Moscow'

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -85,7 +85,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'MSK'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
