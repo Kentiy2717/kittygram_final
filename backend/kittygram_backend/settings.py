79c49638e56d5e571097c6cff65cd31fad2ad3a5
Kentiy2717 <79088565+Kentiy2717@users.noreply.github.com>
2024-08-03 19:12:52 +0700

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-явапявпфвап
+load_dotenv()
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
